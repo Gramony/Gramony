ed38631ae4b34c2032eb499864d9fb39bd3f7bd1
OverflowCat <overflowcat@gmail.com>
2025-12-16 21:09:49 +0800

    feat: video & tgs

---
+++ build-rust.ps1
@@ -0,0 +1,5 @@
+$env:OHOS_NDK_HOME="D:/Sdk/15/"
+$env:RUSTFLAGS = '-Awarnings';
+cd e:\Homogram\features\home\src\main\native\homogrape\
+cargo xtask dist ../../../../libs/arm64-v8a/
+cd E:\Homogram # cd back
